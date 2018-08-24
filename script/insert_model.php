ed80b7553b09acdb0b76cdbb1f709b4230ce649b
ATM-Nicolas <nicolas.prevost@atm-consulting.fr>
2018-08-24 08:47:23 +0200

    FIX : Insert template SQL

--- script/insert_model.php
+++ script/insert_model.php
@@ -21,8 +21,8 @@ if($resql) {
     if(! ($obj = $db->fetch_object($resql)) ) {
         $db->begin();
 
-        $sql_insert = 'INSERT INTO '.MAIN_DB_PREFIX.'c_email_templates(`entity`, `module`, `type_template`, `lang`, `private`, `fk_user`, `datec`, `tms`, `label`, `position`, `active`, `topic`, `content`, `content_lines`, `nodoc`, `enabled`, `joinfiles`)';
-        $sql_insert .= "VALUES (".$conf->entity.", NULL, 'password_reinit', NULL, 0, NULL, NULL, '2018-08-22 12:38:18', 'Réinitialisation mot de passe', 55, 1, 'Compte en ligne - Réinitialisation du mot de passe', 'Bonjour,<br />\n<br />\nVeuillez cliquer sur le lien ci dessous pour r&eacute;initialiser le mot de passe de l&#39;utilisateur \"__ONLINE_ACCOUNT_USER__\"<br />\n__ONLINE_ACCOUNT_LINK__<br />\n<br />\nBien cordialement,<br />\n<br />\n__SIGNATURE__', NULL, NULL, '1', '1')";
+        $sql_insert = 'INSERT INTO '.MAIN_DB_PREFIX.'c_email_templates(`entity`, `module`, `type_template`, `lang`, `private`, `fk_user`, `datec`, `tms`, `label`, `position`, `active`, `topic`, `content`, `content_lines`)';
+        $sql_insert .= "VALUES (".$conf->entity.", NULL, 'password_reinit', NULL, 0, NULL, NULL, '2018-08-22 12:38:18', 'Réinitialisation mot de passe', 55, 1, 'Compte en ligne - Réinitialisation du mot de passe', 'Bonjour,<br />\n<br />\nVeuillez cliquer sur le lien ci dessous pour r&eacute;initialiser le mot de passe de l&#39;utilisateur \"__ONLINE_ACCOUNT_USER__\"<br />\n__ONLINE_ACCOUNT_LINK__<br />\n<br />\nBien cordialement,<br />\n<br />\n__SIGNATURE__', NULL)";
 
         $res = $db->query($sql_insert);
 
